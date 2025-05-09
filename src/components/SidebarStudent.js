78da96f48b44a3232242696687ab6a92345136bd
rnrran <randykamalhusen@gmail.com>
2025-05-09 12:05:58 +0800

    FIX BUILD

--- src/components/SidebarStudent.js
+++ src/components/SidebarStudent.js
@@ -1,6 +1,6 @@
 'use client';
 import Link from 'next/link';
-import '@/styles/sidebarStudent.css'; 
+import '@/styles/sidebarstudent.css'; 
 
 export default function SidebarStudent() {
   return (
